18418c133897722b47b1e4f6034d4777a05b3116
memeToasty <memeToasty@gmail.com>
2021-01-21 19:11:13 +0100

    added time tracking

--- influx.js
+++ influx.js
@@ -1,1 +1,4 @@
+var config = require('./credentials.json');
 const Influx = require('influx');
+
+console.log(config.influx.host);
--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 const express = require('express')
+const fs = require('fs');
 const app = express()
 
+
+//fav icon damit FireFox mir nicht auf den Sack geht
+app.get('/favicon.ico', function (req, res) {
+  console.log(req.query)
+  fs.readFile(process.cwd()+"/favico.ico", function(err,data)
+  {
+    if(err)
+      console.log(err)
+    else
+      res.send(data);
+  });
+})
 
 //main landing page
 app.get('/', function (req, res) {
   console.log(req.query)
+  fs.readFile(process.cwd()+"/index.html", function(err,data)
+  {
+    if(err)
+      console.log(err)
+    else
+      res.send(data.toString());
+  });
 })
 
 
 //timing API
 app.get('/timegoesbrr', function (req, res) {
     console.log(req.query)
+    res.send(200);
 })
   
 app.listen(1337)
