34639c5972f8df67645bce36a0e92fb828a128ea
memeToasty <memeToasty@gmail.com>
2021-01-21 21:27:04 +0100

    added scrambler

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ const express = require('express')
 const fs = require('fs');
 const app = express()
 
+//index.css
+app.get('/index.css', function (req, res) {
+  console.log(req.query)
+  fs.readFile(process.cwd()+"/index.css", function(err,data)
+  {
+    if(err)
+      console.log(err)
+    else
+      res.send(data);
+  });
+})
 
 //fav icon damit FireFox mir nicht auf den Sack geht
 app.get('/favicon.ico', function (req, res) {
